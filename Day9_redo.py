20d36d507f4e1aea1369bc592c694fc15bfb7ad4
Joe <153092825+JoeDev0ps@users.noreply.github.com>
2024-04-18 09:36:27 +0100

    Re-doing some of the 100days in my own time to help with my project...

---
+++ Day9_redo.py
@@ -0,0 +1,11 @@
+programming_dictionary = {
+    "Bug": "An error in a program.",
+    "Function": "A piece of code that you can call repeatedly",
+    "Loop": "The action of doing something repeatedly",
+}
+
+programming_dictionary["Conditional"] = "Code that does something depending if true, or false"
+
+
+for i in programming_dictionary:
+    print(i + ": " + programming_dictionary[i])
