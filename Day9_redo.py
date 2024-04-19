36a74c3640382637339a00afec789fee2cd9e736
Joe <153092825+JoeDev0ps@users.noreply.github.com>
2024-04-19 13:20:02 +0100

    Completed day 9 redo, day 10/11 over the weekend

--- Day9_redo.py
+++ Day9_redo.py
@@ -90,42 +90,113 @@
 # ]
 
 
-# Bid program
-bid_amount = 0
-bidders = {}
-stillBidding = True
+# Bid program - my attempt
+# bid_amount = 0
+# bidders = {}
+# stillBidding = True
 
 
-def highest_bid():
-    highest_bidder = None
-    for bidder, bid in bidders.items():
-        if bid > bid_amount:
-            bid_amount = bid
-            highest_bidder = bidder
-
-    return highest_bidder
+# def highest_bid(bidders):
+#     highest_bidder = None
+#     for bidder in bidders:
+#         highest_bidder = bidders[bidder]
+#         if bidder > highest_bidder:
+#             highest_bidder = bid
+#     print()
+#     print(f"The winning bidder was {bidder}\nThe highest bid was: £{highest_bidder}")
 
       
 
-while stillBidding is True:
+# while stillBidding is True:
 
-  name = input("what's your name? ")
-  bid = int(input("What's your bid? £ "))
+#   name = input("what's your name? ")
+#   bid = int(input("What's your bid? £ "))
 
-  bidders[name] = bid  # Store name and bid amount in dictionary
+#   bidders[name] = bid  # Store name and bid amount in dictionary
 
-  print("Current bidders:")
-  for bidder, bid in bidders.items():
-    print(f"Bidder: {bidder}")  # Print bidder name and bid
+#   print("Current bidders:")
+#   for bidder, bid in bidders.items():
+#     print(f"Bidder: {bidder}")  # Print bidder name and bid
 
-  more_bidding = input(
-      "Any more bids? 'Y' for 'Yes', 'N' for 'No'... ").lower()
+#   more_bidding = input(
+#       "Any more bids? 'Y' for 'Yes', 'N' for 'No'... ").lower()
 
-  if more_bidding == 'n':
-    for bidder, bid in bidders.items():
-      print(f"Bidder: {bidder} \nBid Amount: £{bid}")
-      print()
-      print(f"The winning bid was {highest_bid}")
-    break
-  else:
-    continue
+#   if more_bidding == 'n':
+#     for bidder, bid in bidders.items():
+#       print(f"Bidder: {bidder} \nBid Amount: £{bid}")
+#       highest_bid(bidder)
+#     break
+#   else:
+#     clear()
+#     print(logo)
+#     continue
+
+
+# Re-attempt at bid program - need to refine my highest_bidder function
+# from replit import clear  
+# from art import logo
+logo = '''
+                         ___________
+                         \         /
+                          )_______(
+                          |"""""""|_.-._,.---------.,_.-._
+                          |       | | |               | | ''-.
+                          |       |_| |_             _| |_..-'
+                          |_______| '-' `'---------'` '-'
+                          )"""""""(
+                         /_________\\
+                       .-------------.
+                      /_______________\\
+'''
+print(logo)
+
+bids = {}
+bidding_finished = False
+
+
+# FUNCTION ONE - FAULTY
+# Incorrect update in function 1: Inside the loop in function 1, 
+# it assigns the current bidder's value (bids[bidder]) to highest_bid unconditionally. 
+# This overwrites the previous highest bid value on every iteration, 
+# even if it's not actually higher.
+
+# def highest_bidder(bids):
+#   highest_bid = 0
+#   winner = ""
+#   for bidder in bids:
+#     highest_bid = bids[bidder]
+#     if bids[bidder] > highest_bid:
+#       highest_bid = bids[bidder]
+#       winner = bidder
+#   print(f"The winner is {winner}")
+#   print(f"Their bid was £{highest_bid}")
+
+
+# FUNCTION TWO - WORKING
+def highest_bidder(bidding_record):
+  highest_bid = 0
+  winner = ""
+  for bidder in bidding_record:
+     if bidding_record[bidder] > highest_bid:
+        highest_bid = bidding_record[bidder]
+        winner = bidder
+  print(f"The winner is {winner}")
+  print(f"Their bid was £{highest_bid}")
+
+
+while not bidding_finished:  
+	name = input("What's your name? ")
+	try:
+		price = int(input("What's your bid? £"))
+	except ValueError:
+		print("Not a valid number")
+	bids[name] = price
+	keep_playing = input("Do you want to add bidders? Y or N ").lower()
+	if keep_playing == "n":
+		highest_bidder(bids)
+		bidding_finished = True
+	else:
+	#	clear()
+		print(logo)
+		continue
+  
